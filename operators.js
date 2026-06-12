3f37cd27b7754b3aa2de70f9c88b4e0fe33444b6
vinusri-14 <vinusrip25csa@vetias.ac.in>
2026-06-12 12:07:54 +0530

    added

---
+++ operators.js
@@ -0,0 +1,46 @@
+ //typers of operators
+
+
+ console.log("---------------------------------")
+console.log("Arithmetice operators")
+var a = 20
+var b = 40
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a**b)
+
+
+console.log("---------------------------------")
+console.log("Assignment operators")
+var c = 20
+console.log(c +=10)
+console.log(c -=10)
+console.log(c *=10)
+console.log(c /=10)
+console.log(c %=10)
+console.log(c **=10)
+
+
+console.log("----------------------------------")
+console.log(" Compatison operators")
+var a1 = 20
+var b1 = 40
+console.log(a1==b1)
+console.log(a1!=b1)
+console.log(a1>=b1)
+console.log(a1<=b1)
+console.log(a1>b1)
+console.log(a1<b1)
+
+
+console.log("------------------------------------")
+console.log(" logical operators")
+var c = 10
+var d = 20
+console.log(c>0 && d>0 )
+console.log(c>0 && d<0 )
+console.log(c<0 && d<0 )
+console.log(!(a>0))
---
+++ variables.js
@@ -0,0 +1,13 @@
+//variable declaration var
+var name="VET College"
+console.log(name)
+
+//variable declaration let
+{
+    let age=19
+    console.log(age)
+}
+
+//variable declaration const
+const name1= "oviya"
+console.log(name1)
