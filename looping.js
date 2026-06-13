9e8551312111d2e660126affc85998e2a46027e7
vinusri-14 <vinusrip25csa@vetias.ac.in>
2026-06-13 13:46:36 +0530

    added

---
+++ looping.js
@@ -0,0 +1,34 @@
+for(let i=1 ; i<=10 ; i++)
+   console.log(i+"x2=",i*2)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x3=",i*3)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x4=",i*4)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x5=",i*5)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x6=",i*6)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+console.log(i+"x7=",i*7)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x8=",i*8)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+   console.log(i+"x9=",i*9)
+console.log("======================================")
+
+for(let i=1; i<=10 ; i++)
+console.log(i+"x10=",i*10)
