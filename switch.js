8310cde1a1097ec3988768f27a23b6662f5f6772
vinusri-14 <vinusrip25csa@vetias.ac.in>
2026-06-15 11:08:50 +0530

    added

---
+++ switch.js
@@ -0,0 +1,34 @@
+let days = 8
+switch(days){
+    case 1 :
+        console.log("sunday")
+        break;
+    case 2 :
+        console.log("monday")
+        break;
+        
+    case 3 :
+        console.log("tuesday")
+        break;
+    
+    case 4 :
+        console.log("wednesday")
+        break;
+        
+    case 5 :
+        console.log("thursday")
+        break;
+        
+    case 6 :
+        console.log("friday")
+        break;
+        
+    case 7 :
+        console.log("saturday")
+        break;
+    
+    default :
+        console.log("invail days")      
+
+        
+} 
